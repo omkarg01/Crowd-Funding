5a6646ad18debab83afc2a515d5a7d83cecb8895
omkarg01 <omkargujja01@gmail.com>
2022-11-24 12:06:54 +0530

    contructors and variable declared

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -4,34 +4,39 @@ pragma solidity ^0.8.0;
 contract CrowdFunding {
     // declare all require variables
     // constributors map
+    mapping(address => uint) public contributors;
 
     // manager address
+    address public manager;
 
     // min contro
+    uint public minimumContribution;
 
     // deadline
+    uint public deadline;
 
     // target
+    uint public target;
 
     // raised amt
+    uint public amount;
 
     // no of contros
+    uint public numberOfContributors;
 
     // a constructors which sets the targets, deadline, min contro, manager
+    constructor(uint _target, uint _deadline) {
+        target = _target;
+        deadline = block.timestamp + _deadline;
+        minimumContribution = 100 wei;
+        manager = msg.sender;
+    }
 
     // send eth function
     function sendEth() public payable {
         // check deadline
-
         // check min value
-
         // if no contribution frm sender increase the no of contro
-
         // increase contributors value and raised amt
-
     }
-
-    
-
-
 }
