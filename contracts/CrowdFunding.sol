847ec77ad7592147cf7c8950a85ce74c9a91c779
omkarg01 <omkargujja01@gmail.com>
2022-11-24 11:47:04 +0530

    send eth func

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,5 +2,36 @@
 pragma solidity ^0.8.0;
 
 contract CrowdFunding {
+    // declare all require variables
+    // constributors map
+
+    // manager address
+
+    // min contro
+
+    // deadline
+
+    // target
+
+    // raised amt
+
+    // no of contros
+
+    // a constructors which sets the targets, deadline, min contro, manager
+
+    // send eth function
+    function sendEth() public payable {
+        // check deadline
+
+        // check min value
+
+        // if no contribution frm sender increase the no of contro
+
+        // increase contributors value and raised amt
+
+    }
+
     
+
+
 }
